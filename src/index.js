ff4f3fead4364880263c5f25863a45a2aaa67c4b
juntaozhou05 <350529307@qq.com>
2018-02-13 11:00:51 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {
-    BrowserRouter as Router,
-} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 import ConnectHello from './container/ConnectHello';
 import registerServiceWorker from './registerServiceWorker'
